944b2a87af4a2e34d923b63dd025039242f28937
SiemLuc <48807736+SiemLuc@users.noreply.github.com>
2021-10-14 11:03:20 +0200

    Restructed

---
+++ Dashboard_API/src/test/java/com/dashboard/Dashboard_API/DashboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboard.Dashboard_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Sensor_API/src/main/java/com/sensor/Sensor_API/SensorApiApplication.java
@@ -0,0 +1,13 @@
+package com.sensor.Sensor_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorApiApplication.class, args);
+	}
+
+}
