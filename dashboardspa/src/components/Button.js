61e1a60b49f2dd99e286726ee454b4fffe04efe4
SiemLuc <48807736+SiemLuc@users.noreply.github.com>
2021-12-20 02:00:23 +0100

    This build better work

--- dashboardspa/src/components/Button.js
+++ dashboardspa/src/components/Button.js
@@ -21,7 +21,6 @@ export const Button = ({
     const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
 
-    
     return (
         <button 
             className={`btn ${checkButtonStyle} ${checkButtonSize}`} 
